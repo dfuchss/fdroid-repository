30b446e53509803a6806a5723df657c62afd6a4c
Dominik Fuchß <develop@fuchss.org>
2023-05-20 22:39:08 +0200

    Update element.io.sh

--- scripts/element.io.sh
+++ scripts/element.io.sh
@@ -36,11 +36,8 @@ if [ -f "$META" ]; then
 else
         echo "Meta Data not present. Please update on your own"
 fi
-# Strage Bug of fdroid docker ..
-mv $DEPLOY/.git $DEPLOY/tmp-git
 
-docker run --rm -u $UID -v $DEPLOY:/repo registry.gitlab.com/fdroid/docker-executable-fdroidserver:master update
-
-mv $DEPLOY/tmp-git $DEPLOY/.git
+sudo apt-get update && sudo apt-get install fdroidserver -y
+fdroid update
 
 echo "Finished Deployment"
